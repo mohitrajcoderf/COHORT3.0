54faab2f683246b14279d60907f0e91c0deb9c0d
Mohit raj <mohitraj2546@gmail.com>
2024-09-11 20:19:38 +0530

    done

--- Week3-DOM/script.js
+++ Week3-DOM/script.js
@@ -1,2 +1,8 @@
-const inputElement = document.querySelector("input");
-inputElement.value = "Hello World";
+let ctr = 0;
+function callback(){
+    const  element = document.querySelector('h2')[0];
+    element.innerHTML = ctr;
+    ctr = ctr + 1;
+}
+
+ setInterval(callback, 1000);
